c5d7d0644c09e0f414f4165472562cba088f0c71
MarcinnRogala <mrogala.arkonsoft@gmail.com>
2022-03-03 11:15:02 +0100

    add/login

--- main.php
+++ main.php
@@ -1,17 +1,47 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-require_once "baseconnect.php";
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Your Travel</title>
+</head>
 
-$query =mysqli_connect($hostname, $username, $password, $dbname);
+<body>
+    <div style=" display: flex;
+  margin: 50px auto;
+  width: 1400px;
+  height: 800px;
+  background-color: #f1f2f6;">
+        <div style="width: 40%; margin-top: 50px;">
+            <?php
 
-if (mysqli_connect_errno())
+            require_once "baseconnect.php";
 
-{echo "Error: Can't connect to database";}
+            $query = mysqli_connect($hostname, $username, $password, $dbname);
 
-$result = mysqli_query($query,"SELECT * FROM users");
+            if (mysqli_connect_errno()) {
+                echo "Error: Can't connect to database";
+            }
 
-while($row = mysqli_fetch_array($result))
+            $result = mysqli_query($query, "SELECT * FROM users");
 
-{echo "<span style='font-size: 32px; color: green;'>Your Login: </span>" . $row['Login'] . "<br>" . "<span style='font-size: 32px; color: red;'>Your Password: </span>" . $row['Pass'] . "<br>" . "<span style='font-size: 32px; color: blue;'>Your Email: </span>" . $row['Email']; echo "<br>"; }
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<div style='font-size: 32px; color: green; padding: 15px;'>Your Login: </div>" . $row['Login'] . "<br>" .
+                    "<div style='font-size: 32px; color: red; padding: 15px;'>Your Password: </div>" . $row['Pass'] . "<br>" .
+                    "<div style='font-size: 32px; color: blue; padding: 15px;'>Your Email: </div>" . $row['Email'];
+            }
 
-mysqli_close($query);
+            mysqli_close($query);
+
+            ?>
+        </div>
+        <div>
+            <img src="img/travel.jfif" alt="slide-image" loading="lazy" style="height: 800px; max-width: 100%;" />
+        </div>
+    </div>
+
+</body>
+
+</html>
