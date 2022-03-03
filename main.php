3b95fbc9f598f7633eaee8bd420c863ad4f38aee
MarcinnRogala <mrogala.arkonsoft@gmail.com>
2022-03-03 10:46:43 +0100

    add/login

--- main.php
+++ main.php
@@ -1,4 +1,17 @@
 <?php
 
-if ($rezultat = @$polaczenie->query(
-    sprintf("SELECT * FROM users WHERE Login='%s'")));
+require_once "baseconnect.php";
+
+$query =mysqli_connect($hostname, $username, $password, $dbname);
+
+if (mysqli_connect_errno())
+
+{echo "Error: Can't connect to database";}
+
+$result = mysqli_query($query,"SELECT * FROM users");
+
+while($row = mysqli_fetch_array($result))
+
+{echo "<span style='font-size: 32px; color: green;'>Your Login: </span>" . $row['Login'] . "<br>" . "<span style='font-size: 32px; color: red;'>Your Password: </span>" . $row['Pass'] . "<br>" . "<span style='font-size: 32px; color: blue;'>Your Email: </span>" . $row['Email']; echo "<br>"; }
+
+mysqli_close($query);
